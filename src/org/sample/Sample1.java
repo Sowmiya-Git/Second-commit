6091504bb5f0f0813a3fcf11e3d55b00ad7b1566
Sowmiya-Git <sowmiyaec2015@gmail.com>
2023-07-24 16:14:47 +0530

    My task

---
+++ src/org/sample/Sample1.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
